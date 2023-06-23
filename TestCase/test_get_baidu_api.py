f11aaee8a9056567996317c348050fba5cdcd571
mac <2086179920@qq.com>
2023-06-23 13:42:53 +0800

    添加allure报告

--- TestCase/test_get_baidu_api.py
+++ TestCase/test_get_baidu_api.py
@@ -5,7 +5,7 @@ class TestGetApi:
     def test_get_baidu(self):
         url = "http://www.baidu.com"
         res = reqUtils.ReqUtils().all_request(method = "get", url = url)
-        print(res.text)
+        print(url,res.status_code)
 
     def test_get_baidu_false(self):
         url = "http://www.baidu.com"
@@ -13,8 +13,9 @@ class TestGetApi:
         assert "123" == "456"
 
     def test_get_from_database(self):
-        data = mysqlUtil.MysqlUtil().get_data("test_case", 3)
+        data = mysqlUtil.MysqlUtil().get_data("test_case", 2)
 
         for i in data:
             res = reqUtils.ReqUtils().all_request(method = i[2],url = i[1])
-            print(i[1],"-----------------------")
+            print(i[1],"-----------------------",res.status_code)
+            assert res.status_code == 200
